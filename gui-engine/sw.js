67e5a42db5bf894b45844b2c28294f1163cef866
Omid <omid@sabz.city>
2020-08-12 20:24:09 +0430

    Improve SW by >> - Just cache and handle navigate requests - Add skipWaiting mechanism to load new app just by reload page not close and reopen it - Remove old cache data on every install

--- gui-engine/sw.js
+++ gui-engine/sw.js
@@ -1,55 +1,23 @@
 /* For license and copyright information please see LEGAL file in repository */
 
-const staticCacheName = 'application'
+const staticCacheName = 'app'
 
-self.addEventListener('install', event =>
-    event.waitUntil(
-        // Add home page to cache, So app work offline from app icon in devices!
-        caches.open(staticCacheName)
-            .then(cache => cache.addAll(['/main.html', '/main.js']))
-    )
-)
+self.addEventListener('install', event => {
+    caches.delete(staticCacheName)
+    caches.open(staticCacheName).then(cache => cache.add('/main.html'))
+    self.skipWaiting()
+})
 
 self.addEventListener('fetch', event => {
     // Let the browser do its default thing for non-GET requests.
     if (event.request.method != 'GET') return
 
-    event.respondWith(responseByCacheFirst(event))
+    if (event.request.mode === 'navigate') event.respondWith(responseNavigate(event))
 })
 
-async function responseByCacheFirst(event) {
-    // Try to get the response from a cache.
-    const cache = await caches.open(staticCacheName)
-
-    let res
-
-    if (event.request.mode === 'navigate') {
-        res = await cache.match('/main.html')
-        // If browser cache delete and service worker still get fetch must return 
-        if (!res) return fetch(event.request)
-        return res
-    }
-
-    res = await cache.match(event.request)
-    if (res) {
-        // If we found a match in the cache, return it, but also
-        // update the entry in the cache in the background.
-        cache.add(event.request)
-        return res
-    }
-
-    try {
-        // No cache entry found!
-        res = await fetch(event.request)
-
-        switch (res.status) {
-            case 200:
-                cache.put(event.request, res.clone())
-                return res
-            default: // 400, 404, 500
-                return res
-        }
-    } catch (err) {
-        // Handle no network error situation
-    }
+async function responseNavigate(event) {
+    const res = await caches.match('/main.html')
+    // If browser cache delete and service worker still get fetch must return 
+    if (!res) return fetch('/main.html')
+    return res
 }
