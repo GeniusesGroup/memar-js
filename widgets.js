0d830255a4c0ced451bcb6c983c05a9ec82333a7
Omid <omid@sabz.city>
2020-11-17 15:45:53 +0330

    Add widget global object!

--- widgets.js
+++ widgets.js
@@ -1,3 +1,13 @@
 /* For license and copyright information please see LEGAL file in repository */
 
-const widgets = {}
+const widgets = {
+    poolByID: {}
+}
+
+/**
+ * register given page in pages.poolByID and call page.Init() to initialize page.
+ * @param {object} widget 
+ */
+widgets.RegisterWidget = function(widget) {
+    this.poolByID[widget.ID] = widget
+}
