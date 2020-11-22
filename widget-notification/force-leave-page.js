758101a70263f2938a42249232ab45941ca49bd6
Omid <omid@sabz.city>
2020-11-22 12:49:48 +0330

    Add new force-leave-page widget

---
+++ widget-notification/force-leave-page.js
@@ -0,0 +1,41 @@
+/* For license and copyright information please see LEGAL file in repository */
+
+import '../widgets.js'
+
+/**
+ * 
+ * https://tools.ietf.org/html/bcp47
+ */
+const forceLeavePageWidget = {
+    ID: "force-leave-page",
+    HTML: () => ``,
+    CSS: '',
+    Templates: {}
+}
+widgets.RegisterWidget(forceLeavePageWidget)
+
+forceLeavePageWidget.ConnectedCallback = async function () {
+    if (!this.dialogElement) {
+        this.dialogElement = window.document.createElement("div")
+        this.dialogElement.innerHTML = this.HTML()
+        const style = window.document.createElement("style")
+        style.innerHTML = this.CSS
+        this.dialogElement.appendChild(style)
+
+        window.document.documentElement.appendChild(this.dialogElement)
+    }
+
+    document.getElementById('forceLeavePageDialog').toggle()
+
+    while (this.forceLeave !== true && this.forceLeave !== false) {
+        await time.Sleep(500)
+    }
+
+    document.getElementById('forceLeavePageDialog').toggle()
+    let answer = this.forceLeave
+    this.forceLeave = null
+    return answer
+}
+
+forceLeavePageWidget.DisconnectedCallback = function () {
+}
